937648eb0c657f6e3f84b53d8ba6eca6c392a3da
nikotesla <nikotesla@nikotesla-PC>
2018-11-12 18:04:01 +0800

    优化

--- src/main/java/com/zhtkj/jt808/mapper/VehicleRunMapper.java
+++ src/main/java/com/zhtkj/jt808/mapper/VehicleRunMapper.java
@@ -6,14 +6,9 @@ import org.apache.ibatis.annotations.Param;
 
 import com.baomidou.mybatisplus.mapper.BaseMapper;
 import com.zhtkj.jt808.entity.VehicleRun;
-import com.zhtkj.jt808.vo.req.LocationMsg.LocationInfo;
 
 public interface VehicleRunMapper extends BaseMapper<VehicleRun> {
 
-	int insertVehicleRun(@Param(value = "locationInfo") LocationInfo locationInfo);
-	
-	int updateCarRuntime(@Param(value = "locationInfo") LocationInfo locationInfo);
-	
 	int setVehicleOnline(@Param(value = "licNumber") String licNumber);
 	
 	int setVehicleOffline(@Param(value = "idleTime") String idleTime);
